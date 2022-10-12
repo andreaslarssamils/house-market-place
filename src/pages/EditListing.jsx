fbed7252e9a61d48ec4b9afd0558a5dd191d1485
Andreas Larssamils <larssamils.andreas@gmail.com>
2022-10-12 21:53:36 +0800

    updates

--- src/pages/EditListing.jsx
+++ src/pages/EditListing.jsx
@@ -159,7 +159,7 @@ export default function EditListing() {
           (snapshot) => {
             const progress =
               (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-            // console.log('Upload is ' + progress + '% done');
+            console.log('Upload is ' + progress + '% done');
             switch (snapshot.state) {
               case 'paused':
                 // console.log('Upload is paused');
